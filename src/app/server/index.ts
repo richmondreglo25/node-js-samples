a6015150a9150c6b65892d83e70f0ac89b4230e6
Richmond Reglo <richmondreglo25@gmail.com>
2025-05-27 08:51:41 +0800

    feat: refactor server setup and remove obsolete test files for improved structure and clarity

---
+++ src/app/server/index.ts
@@ -0,0 +1,41 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import swagger from '../config/swagger';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import userRouter from '../routers/userRouter';
+
+class ApiServer {
+    private app: Application;
+    private port: string | undefined;
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.initializeMiddlewares();
+        this.initializeRoutes();
+        this.initializeSwagger();
+    }
+
+    private initializeMiddlewares(): void {
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    private initializeRoutes(): void {
+        this.app.use('/api/users', userRouter);
+    }
+
+    private initializeSwagger(): void {
+        const specs = swaggerJSDoc(swagger);
+        this.app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+    }
+
+    public start(): void {
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        });
+    }
+}
+
+export default ApiServer;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-import express from 'express';
-import cors from 'cors';
-import swagger from './app/config/swagger';
-import swaggerUI from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import userRouter from './app/routers/userRouter';
+import ApiServer from './app/server';
 
-// Constants.
-const PORT = process.env.PORT;
-const app = express();
-
-
-app.use(cors());
-app.use(express.json()); // Express request body parser.
-app.use('/api/users', userRouter);
-
-// Swagger
-const specs = swaggerJSDoc(swagger);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const server = new ApiServer();
+server.start();
+// This is the entry point for the application, where we create an instance of ApiServer and start it.  
